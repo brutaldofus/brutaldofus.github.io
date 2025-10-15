181e6d1588d35a73e3ac91e722e97cd85f2f4f84
brutaldofus <12345678+brutaldofus@users.noreply.github.com>
2025-10-15 19:39:57 +0200

    modif site

--- effect.js
+++ effect.js
@@ -1,16 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
 const container = document.querySelector(".snow-container");
-const numFlakes = 5; // nombre de flocons
+  if (!container) return;
+
+const numFlakes = 3; // nombre de flocons
 
 for(let i = 0; i < numFlakes; i++){
   const flake = document.createElement("div");
   flake.classList.add("snowflake");
 
   // position et taille aléatoire
-  flake.style.left = Math.random() * 125 + "vw";
+  flake.style.left = Math.random() * 100 + "vw";
   flake.style.width = flake.style.height = Math.random() * 8 + 4 + "px";
 
   // vitesse aléatoire
-  flake.style.animationDuration = Math.random() * 10 + 8 + "s";
+  flake.style.animationDuration = Math.random() * 15 + 10 + "s";
 
   container.appendChild(flake);
 }
@@ -27,5 +30,6 @@ if (burger && nav) {
     nav.classList.toggle("open"); // Ajoute ou retire la classe .open
   });
 }
+});
 
 
